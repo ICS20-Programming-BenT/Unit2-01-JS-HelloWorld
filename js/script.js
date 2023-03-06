ce3e188bfb896b0f220da43522613e0e3f6ed712
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-06 16:46:41 +0000

    Updated index.html, style.css, and script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Ben Thomson All rights reserved
+//
+// Created by: Ben Thomson
+// Created on: 03/06/2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays my text on the screen
+  
+  document.getElementById("hello-world").innerHTML = "<p>Hello, and welcome to my first web page with a JavaScript function! This function makes it so that when the button located below is clicked, text appears out of nowhere. I think this is pretty cool, and I am excited to continue adding interactivity to my web pages through JavaScript!</p>"
+}
